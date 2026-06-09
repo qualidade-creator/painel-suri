91a02074cc9fcc7abe2153eb03f6bb939587d650
matheus <qualidade@dispetral.com.br>
2026-06-09 14:17:22 -0300

    Atualização

--- pages/api_produtos.py
+++ pages/api_produtos.py
@@ -1,16 +1,14 @@
-import streamlit as st
+from fastapi import FastAPI
 import pandas as pd
 
-def tela_api_produtos():
+app = FastAPI()
 
-    url = (
-        "https://docs.google.com/spreadsheets/d/e/2PACX-1vTPp5-ZfpJFYC22BlBkdcZ2xavyge91HPP0VK8JbbGfJfW6KNxNv591elxgmPoWBNBZAlmGeYZ_0sXA/pub?gid=0&single=true&output=csv"
-    )
+@app.get("/produtos")
+def produtos():
+
+    url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTPp5-ZfpJFYC22BlBkdcZ2xavyge91HPP0VK8JbbGfJfW6KNxNv591elxgmPoWBNBZAlmGeYZ_0sXA/pub?gid=0&single=true&output=csv"
 
     df = pd.read_csv(url)
 
-    st.json(
-        df.to_dict(
-            orient="records"
-        )
-    )
+    return df.to_dict(orient="records")
+
